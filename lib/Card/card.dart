b8bd8173ce1d504cc953062bc4c99c2bcc875ad2
Arthur <arthurjordao01@gmail.com>
2024-09-22 20:55:00 -0300

    add_card

---
+++ lib/Card/card.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:agenda_phone_flutter/Card/card.dart';
+
+class Card_ extends StatelessWidget {
+  final String nome;
+  final String telefone;
+  final String email;
+  final VoidCallback onTap; // Adicione um callback para o clique
+
+  Card_({
+    required this.nome,
+    required this.telefone,
+    required this.email,
+    required this.onTap, // Inicialize o callback no construtor
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap, // Chama o callback quando o card é clicado
+      child: Card(
+        elevation: 4,
+        margin: EdgeInsets.all(10),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                nome,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(telefone),
+              SizedBox(height: 4),
+              Text(email),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
